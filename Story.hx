b915a2d192f64d53a07afda674f25247660eb98e
MintPaw <jerusanders@gmail.com>
2016-02-02 03:02:06 -0700

    Added some regex

--- Story.hx
+++ Story.hx
@@ -6,13 +6,32 @@ import openfl.Assets;
 class Story extends Sprite
 {
 	var storyText:String;
+	var lables:Map<String, Int> = new Map();
 
-	function new()
-	{
+	function new() {
 		super();
 
-		{ // Load text
-			storyText = Assets.getText("assets/story/main.txt");
+		var labelReg:EReg = new EReg("(label.*)", "ig");
+		storyText = Assets.getText("assets/story/main.txt");
+
+		var lableSplits:Array<String> = labelReg.split(storyText);
+
+	}
+
+	function update():Void
+	{
+		// Maybe I'll use regex. >_>
+		var skipChars:Int = 0;
+		for (i in 0...storyText.length) {
+			var c:String = storyText.charAt(i);
+
+			if (skipChars > 0) {
+				skipChars--;
+			} else if (c == "/") {
+				skipChars = 1;
+			} else if (c == "(") {
+				// skipChars = exec(i);
+			}
 		}
 	}
 }
--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,4 @@
-package;
+package ;
 
 import openfl.display.Sprite;
 
